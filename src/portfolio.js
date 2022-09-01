26b93f61c81c98aa39f4dfbb1eeab082912d0740
Alan Yao <alanyao.training@gmail.com>
2022-09-01 13:30:47 -0500

    remove social

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,7 +12,7 @@ const socialMediaLinks = {
   github: "https://github.com/alaney2",
   linkedin: "https://www.linkedin.com/in/alan-e-yao/",
   gmail: "alaney2@illinois.edu",
-  instagram: "https://www.instagram.com/alan3yao",
+  // instagram: "https://www.instagram.com/alan3yao",
   // twitter: "https://www.twitter.com/ALan_E_Y",
   stackoverflow: "https://stackoverflow.com/users/15632074/alan-yao",
   display: true
